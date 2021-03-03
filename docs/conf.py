e0ac22ce6eb4b06bede5e2597ac12cc8a1833ef7
Florian Kraus <floriankraus@me.com>
2021-03-03 12:23:32 +0100

    Mocking cython import

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,8 @@ extensions = [
         'sphinx.ext.autodoc'
 ]
 
+autodoc_mock_imports = ["Cython"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
