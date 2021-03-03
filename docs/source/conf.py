d523121247620303729d1672da16e14d22f3a36f
Florian Kraus <floriankraus@me.com>
2021-03-03 14:43:58 +0100

    Mocking pryranges import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
+autodoc_mock_imports = ["pyranges"]
 
 # -- Options for HTML output -------------------------------------------------
 
